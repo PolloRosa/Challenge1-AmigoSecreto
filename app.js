e581d1024b29876a5703b21e2d702df3c3b434e6
PolloRosa <accarhuachagua@gmail.com>
2025-08-15 14:56:24 -0500

    Actualizar lista html de amigos

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ function agregarAmigo() {
     if(validarNombreAmigo(nombreAmigo)) {
         amigos.push(nombreAmigo);
         document.getElementById("txtAmigo").value = "";
+        agregarEnLista();
     } else alert("Por favor, inserte un nombre.");
 }
 
 function validarNombreAmigo(amigo) {
     if(amigo === "") return false;
     else return true;
+}
+
+function agregarEnLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for(let index = 0; index < amigos.length; index++) {
+        lista.innerHTML += `<li>${amigos[index]}</li>`;
+    }
 }
