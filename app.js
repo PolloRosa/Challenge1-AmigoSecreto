f187af4720967023f6061f5b5e0866d6c8abb0de
PolloRosa <accarhuachagua@gmail.com>
2025-08-15 14:54:24 -0500

    Agregar amigos al arreglo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let amigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("txtAmigo").value;
+    if(validarNombreAmigo(nombreAmigo)) {
+        amigos.push(nombreAmigo);
+        document.getElementById("txtAmigo").value = "";
+    } else alert("Por favor, inserte un nombre.");
+}
+
+function validarNombreAmigo(amigo) {
+    if(amigo === "") return false;
+    else return true;
+}
